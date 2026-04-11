b013f8b8bb21c4d3febac0f2ad433e311548b73e
Nizam799 <mdamear453@gmail.com>
2026-04-11 09:54:27 -0400

    refactor11

--- .github/scripts/refactor.sh
+++ .github/scripts/refactor.sh
@@ -5,17 +5,30 @@ IFS=$'\n'
 git fetch origin main:origin/main
 
 # Get changed group_vars files from last commit
-ANSIBLE_PATH='^ansible-azure-aad/group_vars/all/.*\.yml$'
+ANSIBLE_PATH='^(ansible-azure-aad/group_vars/all/.*\.yml|ansible-azure-aad/.*\.yml)$'
+ANSIBLE_GROUP_VARS_PATH='^ansible-azure-aad/group_vars/all/.*\.yml$'
+ANSIBLE_PLAYBOOK_PATH='^ansible-azure-aad/[^/]+\.yml$'
 TRIGGERED_PATHS='^(deployments/.*/.*/.*/core_aad/|deployments/.*/.*/.*/core_role_definition/|stacks/.*/aad/|stacks/.*/role_definition/|\.github/workflows/iam-apply\.yml|ansible-azure-aad/)'
+
 ch_files=$(git diff --name-only HEAD^1..HEAD | grep -E "$TRIGGERED_PATHS" || true)
 echo " ch_files:  $ch_files"
+
 echo "aad_groups_changed=true" >> "$GITHUB_OUTPUT"
 matrixArray=()
 if echo "$ch_files" | grep -qvE "$ANSIBLE_PATH"; then
   echo "Non group_vars files detected → running ALL playbooks"
   mapfile -t matrixArray < <(find ./ansible-azure-aad/ -type f -name 'aad-*.yml' | sort)
 else
-  for file in $(echo "$ch_files" | grep -E "$ANSIBLE_PATH"); do
+  mapfile -t existing_playbooks < <(find ansible-azure-aad/ -type f -name 'aad-*.yml')
+  
+  for file in $(echo "$ch_files" | grep -E "$ANSIBLE_PLAYBOOK_PATH"); do
+    if printf '%s\n' "${existing_playbooks[@]}" | grep -qx "$file"; then
+      matrixArray+=("./$file")
+    else
+      echo "Skipping non-existing (deleted) playbook: $file"
+    fi
+  done
+  for file in $(echo "$ch_files" | grep -E "$ANSIBLE_GROUP_VARS_PATH"); do
     echo "Checking changed groups in $file"
 
     # Get old version, extract + normalize aad_groups
